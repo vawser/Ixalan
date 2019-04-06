a12bb3ba4c12cd644ebc9110913da7b7db0d5be8
Vawser <ben.rouncefield@gmail.com>
2019-04-06 17:50:02 +0100

    Initial commit

---
+++ common/defines/00_dawn_of_war.lua
@@ -0,0 +1,8 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.START_DATE = "128.1.1"
+NDefines.NGame.END_DATE = "999.1.1"
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "999.1.1"
+
